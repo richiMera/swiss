ef79d095230f59d659eb248089f8cc3e0b1ddeac
Ferrari Riccardo (DCET/BDO1-IT) <fer1cer@bosch.com>
2024-03-28 09:41:39 +0100

    Primo commit

---
+++ src/components/GridOverlay/index.jsx
@@ -0,0 +1,34 @@
+
+const GridOverlay = ({ show }) => {
+
+
+    return (
+        <>
+            {show && <div style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', zIndex: '400' }}>
+                <div style={{ padding: ' 0 24px', width: '100%', height: '100%', display: 'flex' }}>
+                    {[...Array(12)].map((_, index) => (
+                        <div
+                            key={index}
+
+                            style={{
+                                width: '8.333333333333333%',
+                                height: "100%",
+                                color: 'black',
+                                borderRight: '1px solid red',
+                                borderLeft: index === 0 ? '1px solid red' : '',
+
+                                // transition: "height 0.5s",
+                                // Posizionamento corretto dei div affiancati // Posizionamento corretto dei div affiancati
+                            }}
+                        >
+
+                        </div>
+                    ))}
+                </div>
+            </div>}
+        </>
+    );
+}
+
+
+export default GridOverlay
