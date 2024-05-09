b82d8e03cf93ff636aeaa4f556447c814f6a46d7
Ferrari Riccardo (DCET/BDO1-IT) <fer1cer@bosch.com>
2024-05-09 15:58:31 +0200

    fix info 2

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -164,7 +164,7 @@ const Info = ({ isMobile, isOpen }) => {
                         </Grid>
 
                     </Grid>
-                    <motion.div style={{ position: 'fixed', bottom: isMobile ? '40px' : '16px', right: '16px', left: '16px', color: 'black' }} className='isChildren' variants={variantsItem}>
+                    <motion.div style={{ position: 'fixed', bottom: isMobile ? '110px' : '16px', right: '16px', left: '16px', color: 'black' }} className='isChildren' variants={variantsItem}>
                         <p style={{ fontSize: isMobile ? '12px' : '14px' }}>All material for informational, entertainment and non-profit purposes only. Works appearing on this site are the property of their respective owners and may not be saved, re-transmitted, republished, or reformatted by any means, electronic or mechanical. This site offers broad public access to these materials exclusively for the private, informational and non-commercial use.</p>
                     </motion.div>
                 </motion.div>
