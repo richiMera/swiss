4095660a6aaef5d1ab21f8f4219b38e982625e7e
Ferrari Riccardo (DCET/BDO1-IT) <fer1cer@bosch.com>
2024-05-15 10:34:30 +0200

    fix 3d on hover slider

--- src/components/Detail/index.jsx
+++ src/components/Detail/index.jsx
@@ -18,6 +18,7 @@ const Detail = ({ isMobile, item, setItem, data, index, setIndex
 
 
     const [animationKey, setAnimationKey] = useState(0);
+    const [isHovered, setIsHovered] = useState(false);
     const [variantsScale, setVariantsScale] = useState({
         open: {
             scale: '1',
@@ -123,7 +124,12 @@ const Detail = ({ isMobile, item, setItem, data, index, setIndex
     function handleMouseMove(e) {
         if (mouseMoveInitialized) {
             // setPopCorsList(prevList => [...prevList, { x: e.clientX, y: e.clientY }]);
-            transformImage(e.clientX, e.clientY);
+            if (isHovered) {
+                transformImage(0, 0);
+            } else {
+                transformImage(e.clientX, e.clientY);
+            }
+
         }
     }
 
@@ -165,6 +171,8 @@ const Detail = ({ isMobile, item, setItem, data, index, setIndex
         };
 
     }, [item]);
+
+
 
 
 
@@ -299,7 +307,8 @@ const Detail = ({ isMobile, item, setItem, data, index, setIndex
 
             </motion.div>
 
-            <motion.div style={{ zIndex: '6000', position: isMobile ? 'fixed' : 'absolute', bottom: '24px', right: isMobile ? '50%' : '24px', transform: isMobile ? 'translateX(-50%)' : '', border: '1px solid #2D2D2D', backgroundColor: '#272727', padding: '4px', borderRadius: '50px', display: 'flex', flexDirection: isMobile ? 'row' : 'column', gap: '8px' }} initial={"closed"} animate={item ? "open" : "closed"} variants={variantsYTopIcons}>
+            <motion.div onMouseEnter={() => { if (!isMobile) { setIsHovered(true); } }}
+                onMouseLeave={() => { if (!isMobile) { setIsHovered(false); } }} style={{ zIndex: '6000', position: isMobile ? 'fixed' : 'absolute', bottom: isMobile ? '50px' : '24px', right: isMobile ? '50%' : '24px', border: '1px solid #2D2D2D', backgroundColor: '#272727', padding: '4px', borderRadius: '50px', display: 'flex', flexDirection: isMobile ? 'row' : 'column', gap: '8px' }} initial={"closed"} animate={item ? "open" : "closed"} variants={variantsYTopIcons}>
                 {item && <div className='close-circle-div' onClick={showPrevious} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', cursor: 'pointer', height: '64px', width: '64px' }}>
                     <img draggable={false} src={arrowUp} />
                 </div>}
